2673a82b4e207413b7e084c3cdbc53704c7ffbff
Suyog Mule <smulye10@gmail.com>
2024-08-16 15:43:02 +0530

    change: bg gradient

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="min-h-8 w-full bg-gradient-to-b from-purple-700 to-purple-900 p-4 text-white">
+    <footer className="min-h-8 w-full bg-gradient-to-b from-zinc-700 to-zinc-900 p-4 text-white">
       <div className="container mx-auto">
         <p>2024 All rights not reserved. Please visit again</p>
       </div>
--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
-    <header className="w-full bg-gradient-to-b from-pink-700 to-violet-400 p-2 text-white">
-      <div className="container mx-auto flex flex-wrap items-center justify-between gap-2">
+    <header className="w-full bg-gradient-to-b from-zinc-900 to-zinc-700 p-4 text-white">
+      <div className="ga container mx-auto flex flex-wrap items-center justify-between">
         <h1 className="text-4xl font-bold tracking-widest">Catopedia</h1>
         <input
           type="search"
