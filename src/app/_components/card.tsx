d215476da42b8f4b4539a113d91bfea9ce284f56
Suyog Mule <smulye10@gmail.com>
2024-08-18 13:32:43 +0530

    feat: load more images on scroll

--- src/app/_components/card.tsx
+++ src/app/_components/card.tsx
@@ -1,35 +1,48 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
 import type { PostCard } from "~/types";
 
-export default function Card(card: PostCard) {
-  return (
-    <div className="flex w-full flex-col gap-2 rounded-xl border-2 border-neutral-700 bg-neutral-600 p-3">
-      <Link href={`/${card.id}`} about="Details of cat">
-        <Image
-          src={card.image}
-          alt="cat img"
-          width={500}
-          height={400}
-          className="h-52 w-full object-cover object-center"
-        />
-        <table>
-          <tbody>
-            <tr className="bg-slate-600">
-              <td className="px-2">Breed </td>
-              <td className="px-2">{card.breed}</td>
-            </tr>
-            <tr className="bg-gray-600">
-              <td className="px-2">Origin </td>
-              <td className="px-2">{card.origin}</td>
-            </tr>
-            <tr className="bg-zinc-600">
-              <td className="px-2">Temperment</td>
-              <td className="px-2">{card.temperment}</td>
-            </tr>
-          </tbody>
-        </table>
-      </Link>
-    </div>
-  );
-}
+import React from "react";
+
+const Card = React.memo(
+  React.forwardRef<HTMLDivElement, PostCard>((card, ref) => {
+    return (
+      <div
+        ref={ref}
+        className="flex w-full flex-col gap-2 rounded-xl border-2 border-neutral-700 bg-neutral-600 p-3"
+      >
+        <Link href={`/${card.id}`} about="Details of cat">
+          <Image
+            src={card.image}
+            alt="cat img"
+            width={500}
+            height={400}
+            className="h-52 w-full object-cover object-center"
+          />
+          <table>
+            <tbody>
+              <tr className="bg-slate-600">
+                <td className="px-2">Breed </td>
+                <td className="px-2">{card.breed}</td>
+              </tr>
+              <tr className="bg-gray-600">
+                <td className="px-2">Origin </td>
+                <td className="px-2">{card.origin}</td>
+              </tr>
+              <tr className="bg-zinc-600">
+                <td className="px-2">Temperment</td>
+                <td className="px-2">{card.temperment}</td>
+              </tr>
+            </tbody>
+          </table>
+        </Link>
+      </div>
+    );
+  }),
+);
+
+Card.displayName = "Cat Teaser Card";
+
+export default Card;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,94 @@
-import { api, HydrateClient } from "~/trpc/server";
-import Card from "./_components/card";
+"use client";
 
-export default async function Home() {
-  "use client";
-  const data = await api.cats.images({ page: 0, limit: 10 });
+import { api } from "~/trpc/react";
+import Card from "./_components/card";
+import React, { useState, useEffect, useRef, useCallback } from "react";
+import type { PostCard } from "~/types";
+
+export default function Home() {
+  const [cards, setCards] = useState<PostCard[]>([]);
+  const [offset, setOffset] = useState(0);
+  const [hasMore, setHasMore] = useState(true);
+  const [isFetching, setIsFetching] = useState<boolean>(false);
+  const observer = useRef<IntersectionObserver | null>(null);
+
+  // Fetch Data from tRPC
+  const { data, isLoading, error } = api.cats.images.useQuery(
+    {
+      page: offset,
+      limit: 10,
+    },
+    {
+      enabled: !isFetching && hasMore,
+    },
+  );
+
+  useEffect(() => {
+    if (data) {
+      if (data.length > 0) {
+        setCards((prevCards) => [...prevCards, ...data]);
+      } else {
+        setHasMore(false);
+      }
+      setIsFetching(false);
+    }
+  }, [data]);
+
+  // terminate fetching after 6 Pages.
+  useEffect(() => {
+    if (offset >= 5) {
+      setHasMore(false);
+    }
+  }, [offset]);
+
+  const lastCardRef = useCallback(
+    (node: HTMLDivElement) => {
+      if (isLoading) return;
+      if (observer?.current) observer.current.disconnect();
+
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0]?.isIntersecting && hasMore) {
+          setOffset((prevOffset) => prevOffset + 1);
+        }
+      });
+
+      if (node) observer.current.observe(node);
+    },
+    [isLoading, hasMore],
+  );
+
   return (
-    <HydrateClient>
-      <main className="min-h-screen bg-zinc-700 p-2 text-white">
-        <div className="container mx-auto grid min-h-screen grid-cols-1 flex-col gap-x-3 gap-y-4 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
-          {data.map((card) => (
-            <Card
-              key={card.id}
-              id={card.id}
-              image={card.image}
-              breed={card.breed}
-              origin={card.origin}
-              temperment={card.temperment}
-            />
-          ))}
-        </div>
-      </main>
-    </HydrateClient>
+    <main className="min-h-screen bg-zinc-700 p-2 text-white">
+      <div className="container mx-auto grid min-h-screen grid-cols-1 flex-col gap-x-3 gap-y-4 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
+        {cards.map((card, idx) => {
+          if (idx === cards.length - 1) {
+            return (
+              <Card
+                key={card.id}
+                id={card.id}
+                image={card.image}
+                breed={card.breed}
+                origin={card.origin}
+                temperment={card.temperment}
+                ref={lastCardRef}
+              />
+            );
+          } else {
+            return (
+              <Card
+                key={card.id}
+                id={card.id}
+                image={card.image}
+                breed={card.breed}
+                origin={card.origin}
+                temperment={card.temperment}
+              />
+            );
+          }
+        })}
+        {isLoading && <p>Loading More cats...</p>}
+        {error && <p>Error loading teasers: {error.message}</p>}
+      </div>
+    </main>
   );
 }
